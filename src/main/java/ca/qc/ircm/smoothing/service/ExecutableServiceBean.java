6b9510f1d0cc8bc9da49001d7c68d1766ee8437f
Christian Poitras <christian.poitras@ircm.qc.ca>
2015-07-16 14:32:21 -0400

    Added executable for Mac.

--- src/main/java/ca/qc/ircm/smoothing/service/ExecutableServiceBean.java
+++ src/main/java/ca/qc/ircm/smoothing/service/ExecutableServiceBean.java
@@ -31,68 +31,70 @@ import ca.qc.ircm.smoothing.OperatingSystemService;
  * Default implementation of {@link ExecutableService}.
  */
 public class ExecutableServiceBean implements ExecutableService {
-	private final static Pattern PROGRESS_PATTERN = Pattern.compile("\\s+(\\d+)%");
+    private final static Pattern PROGRESS_PATTERN = Pattern.compile("\\s+(\\d+)%");
 
-	private static class SmoothingEventGenerator extends LogOutputStream {
-		private final SmoothingEventListener listener;
+    private static class SmoothingEventGenerator extends LogOutputStream {
+	private final SmoothingEventListener listener;
 
-		public SmoothingEventGenerator(SmoothingEventListener listener) {
-			this.listener = listener;
-		}
-
-		@Override
-		protected void processLine(String line, int level) {
-			Matcher matcher = PROGRESS_PATTERN.matcher(line);
-			if (matcher.matches()) {
-				listener.processProgress(Double.valueOf(matcher.group(1)) / 100);
-			}
-		}
-	}
-
-	private final Logger logger = LoggerFactory.getLogger(ExecutableServiceBean.class);
-	private final ResourceBundle bundle = ResourceBundle.getBundle(ExecutableService.class.getName());
-	private final OperatingSystemService operatingSystemService;
-	private Provider<Executor> executorProvider;
-
-	@Inject
-	protected ExecutableServiceBean(OperatingSystemService operatingSystemService, Provider<Executor> executorProvider) {
-		this.operatingSystemService = operatingSystemService;
-		this.executorProvider = executorProvider;
+	public SmoothingEventGenerator(SmoothingEventListener listener) {
+	    this.listener = listener;
 	}
 
 	@Override
-	public void smoothing(File parameters, SmoothingEventListener listener) throws IOException {
-		File directory = new File(SystemUtils.getUserHome(), "smoothing");
-		if (!directory.exists() && !directory.mkdir()) {
-			throw new IOException("Could not create directory " + directory);
-		}
-		File executable = extractExecutable(directory);
-		CommandLine commandLine = new CommandLine(executable);
-		commandLine.addArgument(parameters.getAbsolutePath());
-		Executor executor = executorProvider.get();
-		if (listener != null) {
-			OutputStream output = new TeeOutputStream(System.out, new SmoothingEventGenerator(listener));
-			executor.setStreamHandler(new PumpStreamHandler(output, System.err));
-		}
-		executor.setWorkingDirectory(directory);
-		logger.info("Executing: {}", commandLine);
-		int exitValue = executor.execute(commandLine);
-		if (executor.isFailure(exitValue)) {
-			throw new IOException("Incorrect return value " + exitValue);
-		}
+	protected void processLine(String line, int level) {
+	    Matcher matcher = PROGRESS_PATTERN.matcher(line);
+	    if (matcher.matches()) {
+		listener.processProgress(Double.valueOf(matcher.group(1)) / 100);
+	    }
 	}
+    }
 
-	private File extractExecutable(File directory) throws IOException {
-		File executable = new File(directory, "smoothing.exe");
-		OperatingSystem operatingSystem = operatingSystemService.currentOS();
-		boolean is64bits = operatingSystemService.is64bit(operatingSystem);
-		String resource = "/executables/"
-				+ bundle.getString("smoothing." + operatingSystem.name() + "." + (is64bits ? "64" : "32"));
-		logger.trace("Extracting {} to {}", resource, executable);
-		try (InputStream input = new BufferedInputStream(getClass().getResourceAsStream(resource));
-				OutputStream output = new BufferedOutputStream(new FileOutputStream(executable))) {
-			IOUtils.copy(input, output);
-		}
-		return executable;
+    private final Logger logger = LoggerFactory.getLogger(ExecutableServiceBean.class);
+    private final ResourceBundle bundle = ResourceBundle.getBundle(ExecutableService.class.getName());
+    private final OperatingSystemService operatingSystemService;
+    private Provider<Executor> executorProvider;
+
+    @Inject
+    protected ExecutableServiceBean(OperatingSystemService operatingSystemService,
+	    Provider<Executor> executorProvider) {
+	this.operatingSystemService = operatingSystemService;
+	this.executorProvider = executorProvider;
+    }
+
+    @Override
+    public void smoothing(File parameters, SmoothingEventListener listener) throws IOException {
+	File directory = new File(SystemUtils.getUserHome(), "smoothing");
+	if (!directory.exists() && !directory.mkdir()) {
+	    throw new IOException("Could not create directory " + directory);
 	}
+	File executable = extractExecutable(directory);
+	CommandLine commandLine = new CommandLine(executable);
+	commandLine.addArgument(parameters.getAbsolutePath());
+	Executor executor = executorProvider.get();
+	if (listener != null) {
+	    OutputStream output = new TeeOutputStream(System.out, new SmoothingEventGenerator(listener));
+	    executor.setStreamHandler(new PumpStreamHandler(output, System.err));
+	}
+	executor.setWorkingDirectory(directory);
+	logger.info("Executing: {}", commandLine);
+	int exitValue = executor.execute(commandLine);
+	if (executor.isFailure(exitValue)) {
+	    throw new IOException("Incorrect return value " + exitValue);
+	}
+    }
+
+    private File extractExecutable(File directory) throws IOException {
+	File executable = new File(directory, "smoothing.exe");
+	OperatingSystem operatingSystem = operatingSystemService.currentOS();
+	boolean is64bits = operatingSystemService.is64bit(operatingSystem);
+	String resource = "/executables/"
+		+ bundle.getString("smoothing." + operatingSystem.name() + "." + (is64bits ? "64" : "32"));
+	logger.trace("Extracting {} to {}", resource, executable);
+	try (InputStream input = new BufferedInputStream(getClass().getResourceAsStream(resource));
+		OutputStream output = new BufferedOutputStream(new FileOutputStream(executable))) {
+	    IOUtils.copy(input, output);
+	}
+	executable.setExecutable(true);
+	return executable;
+    }
 }
--- src/test/java/ca/qc/ircm/smoothing/service/ExecutableServiceBeanTest.java
+++ src/test/java/ca/qc/ircm/smoothing/service/ExecutableServiceBeanTest.java
@@ -36,99 +36,117 @@ import ca.qc.ircm.smoothing.test.config.TestRunnerLog4J;
  */
 @RunWith(TestRunnerLog4J.class)
 public class ExecutableServiceBeanTest {
-	private ExecutableServiceBean executableServiceBean;
-	@Mock
-	private SmoothingEventListener listener;
-	@Mock
-	private OperatingSystemService operatingSystemService;
-	@Mock
-	private Provider<Executor> executorProvider;
-	@Mock
-	private Executor executor;
-	@Captor
-	private ArgumentCaptor<ExecuteStreamHandler> streamHandlerCaptor;
-	@Captor
-	private ArgumentCaptor<CommandLine> commandLineCaptor;
+    private ExecutableServiceBean executableServiceBean;
+    @Mock
+    private SmoothingEventListener listener;
+    @Mock
+    private OperatingSystemService operatingSystemService;
+    @Mock
+    private Provider<Executor> executorProvider;
+    @Mock
+    private Executor executor;
+    @Captor
+    private ArgumentCaptor<ExecuteStreamHandler> streamHandlerCaptor;
+    @Captor
+    private ArgumentCaptor<CommandLine> commandLineCaptor;
 
-	@Before
-	public void beforeTest() {
-		executableServiceBean = new ExecutableServiceBean(operatingSystemService, executorProvider);
-		when(operatingSystemService.currentOS()).thenReturn(OperatingSystem.WINDOWS);
-		when(operatingSystemService.is64bit(any(OperatingSystem.class))).thenReturn(false);
-		when(executorProvider.get()).thenReturn(executor);
-	}
+    @Before
+    public void beforeTest() {
+	executableServiceBean = new ExecutableServiceBean(operatingSystemService, executorProvider);
+	when(operatingSystemService.currentOS()).thenReturn(OperatingSystem.WINDOWS);
+	when(operatingSystemService.is64bit(any(OperatingSystem.class))).thenReturn(false);
+	when(executorProvider.get()).thenReturn(executor);
+    }
 
-	@Test
-	public void smoothing() throws Throwable {
-		File parameters = new File("parameters.txt");
-		StringBuilder processOutput = new StringBuilder();
-		processOutput.append("Start\n");
-		processOutput.append("\t10%\n");
-		processOutput.append("\t25%\n");
-		processOutput.append("\t68%\n");
-		processOutput.append("\t100%\n");
-		processOutput.append("End\n");
-		final ByteArrayInputStream input = new ByteArrayInputStream(processOutput.toString().getBytes(
-				Charset.defaultCharset()));
-		doAnswer(new Answer<Void>() {
-			@Override
-			public Void answer(InvocationOnMock invocation) throws Throwable {
-				ExecuteStreamHandler streamHandler = (ExecuteStreamHandler) invocation.getArguments()[0];
-				streamHandler.setProcessOutputStream(input);
-				streamHandler.start();
-				return null;
-			}
-		}).when(executor).setStreamHandler(any(ExecuteStreamHandler.class));
+    @Test
+    public void smoothing() throws Throwable {
+	File parameters = new File("parameters.txt");
+	StringBuilder processOutput = new StringBuilder();
+	processOutput.append("Start\n");
+	processOutput.append("\t10%\n");
+	processOutput.append("\t25%\n");
+	processOutput.append("\t68%\n");
+	processOutput.append("\t100%\n");
+	processOutput.append("End\n");
+	final ByteArrayInputStream input = new ByteArrayInputStream(
+		processOutput.toString().getBytes(Charset.defaultCharset()));
+	doAnswer(new Answer<Void>() {
+	    @Override
+	    public Void answer(InvocationOnMock invocation) throws Throwable {
+		ExecuteStreamHandler streamHandler = (ExecuteStreamHandler) invocation.getArguments()[0];
+		streamHandler.setProcessOutputStream(input);
+		streamHandler.start();
+		return null;
+	    }
+	}).when(executor).setStreamHandler(any(ExecuteStreamHandler.class));
 
-		executableServiceBean.smoothing(parameters, listener);
+	executableServiceBean.smoothing(parameters, listener);
 
-		verify(executor).setStreamHandler(streamHandlerCaptor.capture());
-		verify(executorProvider).get();
-		verify(executor).setWorkingDirectory(new File(SystemUtils.getUserHome(), "smoothing"));
-		verify(executor).execute(commandLineCaptor.capture());
-		CommandLine commandLine = commandLineCaptor.getValue();
-		File executable = new File(commandLine.getExecutable());
-		assertEquals(true, executable.exists());
-		assertEquals(true, executable.canExecute());
-		// Validate events.
-		Thread.sleep(200);
-		streamHandlerCaptor.getValue().stop();
-		verify(listener).processProgress(0.1);
-		verify(listener).processProgress(0.25);
-		verify(listener).processProgress(0.68);
-		verify(listener).processProgress(1.0);
-	}
+	verify(executor).setStreamHandler(streamHandlerCaptor.capture());
+	verify(executorProvider).get();
+	verify(executor).setWorkingDirectory(new File(SystemUtils.getUserHome(), "smoothing"));
+	verify(executor).execute(commandLineCaptor.capture());
+	CommandLine commandLine = commandLineCaptor.getValue();
+	File executable = new File(commandLine.getExecutable());
+	assertEquals(true, executable.exists());
+	assertEquals(true, executable.canExecute());
+	// Validate events.
+	Thread.sleep(200);
+	streamHandlerCaptor.getValue().stop();
+	verify(listener).processProgress(0.1);
+	verify(listener).processProgress(0.25);
+	verify(listener).processProgress(0.68);
+	verify(listener).processProgress(1.0);
+    }
 
-	@Test
-	public void smoothing_Windows32() throws Throwable {
-		File parameters = new File("parameters.txt");
+    @Test
+    public void smoothing_Windows32() throws Throwable {
+	File parameters = new File("parameters.txt");
 
-		executableServiceBean.smoothing(parameters, listener);
+	executableServiceBean.smoothing(parameters, listener);
 
-		verify(executor).execute(commandLineCaptor.capture());
-		CommandLine commandLine = commandLineCaptor.getValue();
-		File executable = new File(commandLine.getExecutable());
-		assertEquals(true, executable.exists());
-		assertEquals(true, executable.canExecute());
-		String executableResource = "/executables/smoothing_32.exe";
-		File expectedExecutable = new File(this.getClass().getResource(executableResource).toURI());
-		assertEquals(FileUtils.checksumCRC32(expectedExecutable), FileUtils.checksumCRC32(executable));
-	}
+	verify(executor).execute(commandLineCaptor.capture());
+	CommandLine commandLine = commandLineCaptor.getValue();
+	File executable = new File(commandLine.getExecutable());
+	assertEquals(true, executable.exists());
+	assertEquals(true, executable.canExecute());
+	String executableResource = "/executables/smoothing_32.exe";
+	File expectedExecutable = new File(this.getClass().getResource(executableResource).toURI());
+	assertEquals(FileUtils.checksumCRC32(expectedExecutable), FileUtils.checksumCRC32(executable));
+    }
 
-	@Test
-	public void smoothing_Windows64() throws Throwable {
-		when(operatingSystemService.is64bit(any(OperatingSystem.class))).thenReturn(true);
-		File parameters = new File("parameters.txt");
+    @Test
+    public void smoothing_Windows64() throws Throwable {
+	when(operatingSystemService.is64bit(any(OperatingSystem.class))).thenReturn(true);
+	File parameters = new File("parameters.txt");
 
-		executableServiceBean.smoothing(parameters, listener);
+	executableServiceBean.smoothing(parameters, listener);
 
-		verify(executor).execute(commandLineCaptor.capture());
-		CommandLine commandLine = commandLineCaptor.getValue();
-		File executable = new File(commandLine.getExecutable());
-		assertEquals(true, executable.exists());
-		assertEquals(true, executable.canExecute());
-		String executableResource = "/executables/smoothing_64.exe";
-		File expectedExecutable = new File(this.getClass().getResource(executableResource).toURI());
-		assertEquals(FileUtils.checksumCRC32(expectedExecutable), FileUtils.checksumCRC32(executable));
-	}
+	verify(executor).execute(commandLineCaptor.capture());
+	CommandLine commandLine = commandLineCaptor.getValue();
+	File executable = new File(commandLine.getExecutable());
+	assertEquals(true, executable.exists());
+	assertEquals(true, executable.canExecute());
+	String executableResource = "/executables/smoothing_64.exe";
+	File expectedExecutable = new File(this.getClass().getResource(executableResource).toURI());
+	assertEquals(FileUtils.checksumCRC32(expectedExecutable), FileUtils.checksumCRC32(executable));
+    }
+
+    @Test
+    public void smoothing_Mac64() throws Throwable {
+	when(operatingSystemService.currentOS()).thenReturn(OperatingSystem.MAC);
+	when(operatingSystemService.is64bit(any(OperatingSystem.class))).thenReturn(true);
+	File parameters = new File("parameters.txt");
+
+	executableServiceBean.smoothing(parameters, listener);
+
+	verify(executor).execute(commandLineCaptor.capture());
+	CommandLine commandLine = commandLineCaptor.getValue();
+	File executable = new File(commandLine.getExecutable());
+	assertEquals(true, executable.exists());
+	assertEquals(true, executable.canExecute());
+	String executableResource = "/executables/smoothing_mac_64";
+	File expectedExecutable = new File(this.getClass().getResource(executableResource).toURI());
+	assertEquals(FileUtils.checksumCRC32(expectedExecutable), FileUtils.checksumCRC32(executable));
+    }
 }
